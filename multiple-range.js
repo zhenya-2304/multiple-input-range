22f756c8d7f2db1b56664fecd80accc7890bf75c
zhenya-2304 <dereviankozhenya@gmail.com>
2019-08-30 06:07:34 +0300

    Add files via upload

---
+++ multiple-range.js
@@ -0,0 +1,100 @@
+function getCoords(elem) {
+	/*Получаем координаты относительно окна браузера*/
+	let coords = elem.getBoundingClientRect();
+	/*Высчитываем значения координат относительно документа, вычисляя прокрутку документа*/
+	return {
+		top : coords.top + window.pageYOffset,
+		left : coords.left + window.pageXOffset,
+		leftX: coords.left,
+		rigth : coords.left + window.pageXOffset + coords.width,
+		bottom : coords.top + window.pageYOffset + coords.height,
+		width : coords.width
+	}
+}
+
+function moveRange (elem) {
+	/*Находим нужный элемент по классу или id*/
+	var coords = getCoords(elem);
+
+	/*Определяем зону окрашывания*/
+	var colorRange = elem.parentElement.children[1];
+	var f;//устанавливаем флаг для определения мин или макс элемента
+
+	/*Определяем второй ползунок и родителя*/
+	var parent = {}
+		parent.element = elem.parentElement;
+		parent.coords = getCoords(parent.element);
+
+	var block2 = {}
+	if (elem.classList.contains('block-min')) {
+		block2.element = elem.parentElement.children[2];
+		block2.coords = getCoords(block2.element);
+		f=0;
+	} else {
+		block2.element = elem.parentElement.children[0];
+		block2.coords = getCoords(block2.element);
+		f=1;
+	}
+
+	/*Делаем индикатор над ползунком*/
+    var indicator = document.createElement('div');
+    if (elem.children.length){
+       	elem.innerHTML = '';
+    }
+	elem.append(indicator);
+
+	document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+    /*выключаем браузерное событие DaD*/
+    elem.ondragstart = function(){
+        return false;
+    }
+
+	function onMouseMove(e) {
+
+		/*Определяем смещение влево*/
+        e.preventDefault();//предотвратить запуск выделения элементов
+
+        /*Устанавливаем границы движения ползунка*/
+        let newLeft = e.clientX - parent.coords.leftX;
+        let rigthEdge = parent.coords.width - coords.width;
+
+        if (newLeft<0) {
+        	newLeft = 0;
+        }else if (newLeft > rigthEdge) {
+        	newLeft = rigthEdge;
+        }
+        if (f == 0 && e.clientX > block2.coords.left-block2.coords.width) {
+        	newLeft = block2.coords.left - block2.coords.width - 5 - parent.coords.leftX;
+        }else if (f == 1 && e.clientX < block2.coords.rigth + 5) {
+        	newLeft = block2.coords.rigth + 5 - parent.coords.leftX;
+        }
+        /*устанавливаем отступ нашему элементу*/
+    	elem.style.left = newLeft + 'px';
+
+    	/*Выводим значение над ползунком*/
+    	indicator.style.position = 'absolute';
+    	indicator.style.fontSize = "14px";
+    	indicator.style.left = - coords.width/2 + "px";
+    	indicator.style.top = parseFloat(window.getComputedStyle(elem).getPropertyValue('top')) - 10 +"px";
+    	indicator.innerHTML = newLeft +"";
+
+
+    	/*Делаем цветную плашечку диапазона выбора*/
+    	if (f == 0) {
+	    	colorRange.style.left = newLeft + coords.width + "px";
+	    	colorRange.style.width = block2.coords.left - getCoords(elem).left - coords.width + "px";
+    	}  else {
+    		colorRange.style.left = block2.coords.left - parent.coords.leftX + "px";
+    		colorRange.style.width = getCoords(elem).left - block2.coords.left + "px";
+    	}    
+	}
+
+	function onMouseUp() {
+	    document.removeEventListener('mouseup', onMouseUp);
+	    document.removeEventListener('mousemove', onMouseMove);
+	}
+}
+
+
